8e35f00628499b653dd3e2bbfca4688947558f2e
Aymenbachiri <aymen.bachiri99@gmail.com>
2024-01-17 18:45:24 +0100

    add logo

---
+++ src/components/VideoCall.jsx
@@ -0,0 +1,11 @@
+const VideoCall = () => {
+  return (
+    <section className="w-[400px] h-[400px] absolute bg-white mt-2 rounded-lg">
+      <div className="">
+        <h1>Video</h1>
+      </div>
+    </section>
+  );
+};
+
+export default VideoCall;
